24cf3e86fb172227db17d976447613f97d3bc111
firstcommit <rosenich@badinhs.org>
2016-10-04 08:24:48 -0400

    second view controller added

--- MyFavoriteEmojis/ViewController.swift
+++ MyFavoriteEmojis/ViewController.swift
@@ -5,14 +5,46 @@
 //  Created by BHSRam5 on 9/30/16.
 //  Copyright © 2016 BHSRam5. All rights reserved.
 //
+//Table View
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var coolTableView: UITableView!
+    
+    
+    var emoji=["😎 sunglasses","🤓 glasses","🤔 thinking","😱 scared","😬 startled","😇 good","😤 angry"]
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableView.dataSource=self
+        //that is used to define the table
+        coolTableView.delegate=self
+        //and contents
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emoji.count
+
+        //showing number of rows as interger
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell=UITableViewCell()
+        cell.textLabel?.text=emoji[indexPath.row]
+        return cell
+        
     }
 
     override func didReceiveMemoryWarning() {
